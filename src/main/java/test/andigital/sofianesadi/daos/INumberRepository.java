f5cfe0b4bc32973c521041306c9eabf557a46e9e
SofianeSadiPro <pro.sofiane.sadi@gmail.com>
2018-05-25 04:35:48 +0100

    Adding the NumberRepository interface.

---
+++ src/main/java/test/andigital/sofianesadi/daos/INumberRepository.java
@@ -0,0 +1,37 @@
+package test.andigital.sofianesadi.daos;
+
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import test.andigital.sofianesadi.entities.NumberEntity;
+
+/**
+ * Number repository contract
+ */
+public interface INumberRepository {
+    /**
+     * Find all numbers
+     * @return List of numbers if any, empty list otherwise.
+     */
+    Flux<NumberEntity> findAll();
+
+    /**
+     * Find a number by ID.
+     * @param id The number ID
+     * @return List of numbers if matches, empty list otherwise.
+     */
+    Mono<NumberEntity> findById(Long id);
+
+    /**
+     * Find by customer id
+     * @param customerId The customer ID
+     * @return List of numbers for the given customer id if found, empty list otherwise.
+     */
+    Flux<NumberEntity> findByCustomerId(Long customerId);
+
+    /**
+     * Save a number entity
+     * @param numberEntity Number entity to save
+     * @return Persisted NumberEntity.
+     */
+    Mono<NumberEntity> save(NumberEntity numberEntity);
+}
